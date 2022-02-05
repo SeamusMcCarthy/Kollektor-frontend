32582668bacdd6da2a1e992942e30f8a975ecf44
Seamus McCarthy <47305550+spoderman01@users.noreply.github.com>
2022-02-05 14:28:43 +0000

    Added shared components, site images for categories and updated landing page

--- src/App.js
+++ src/App.js
@@ -1,33 +1,36 @@
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import React from 'react';
-import { BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom'
-import SiteHeader from './components/SiteHeader';
-import LandingPage from './pages/LandingPage';
-import CategoryPage from './pages/CategoryPage';
-
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+import SiteHeader from "./components/SiteHeader";
+import Categories from "./pages/Categories";
+import CategoryPage from "./pages/CategoryPage";
 
 function App() {
-
   const theme = createTheme();
   return (
-  <Router>
-    <ThemeProvider theme={theme}>
-    <SiteHeader />
-    <main>
-      <Switch>
-        <Route path="/" exact>
-          <LandingPage />  
-        </Route>
-        <Route exact path="/home" component={LandingPage} />
-        
-        <Route path="/category/:id" component={CategoryPage} />
-        
-        <Redirect to="/" />
-      </Switch>
-    </main>
-    </ThemeProvider>
-  </Router>)
-  ;
+    <Router>
+      <ThemeProvider theme={theme}>
+        <SiteHeader />
+        <main>
+          <Switch>
+            <Route path="/" exact>
+              <Categories />
+            </Route>
+            <Route exact path="/home" component={Categories} />
+
+            <Route path="/category/:id" component={CategoryPage} />
+
+            <Redirect to="/" />
+          </Switch>
+        </main>
+      </ThemeProvider>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/shared/components/FormElements/Input.js
@@ -0,0 +1,127 @@
+import React, { useReducer, useEffect } from "react";
+import "./Input.css";
+import { validate } from "../../util/validators";
+
+function inputReducer(state, action) {
+  switch (action.type) {
+    case "CHANGE":
+      return {
+        ...state,
+        value: action.val,
+        isValid: validate(action.val, action.validators),
+      };
+    case "TOUCH":
+      return {
+        ...state,
+        isTouched: true,
+      };
+    default:
+      return state;
+  }
+}
+
+function Input(props) {
+  const [inputState, dispatch] = useReducer(inputReducer, {
+    value: props.initialValue || "",
+    isValid: props.initialValid || false,
+    isTouched: false,
+  });
+
+  const { id, onInput } = props;
+  const { value, isValid } = inputState;
+  useEffect(() => {
+    onInput(id, value, isValid);
+  }, [id, onInput, value, isValid]);
+
+  function changeHandler(event) {
+    dispatch({
+      type: "CHANGE",
+      val: event.target.value,
+      validators: props.validators,
+    });
+  }
+
+  function touchHandler(event) {
+    dispatch({ type: "TOUCH" });
+  }
+
+  const element =
+    props.element === "input" ? (
+      <input
+        id={props.id}
+        type={props.type}
+        placeholder={props.placeholder}
+        onChange={changeHandler}
+        onBlur={touchHandler}
+        value={inputState.value}
+      />
+    ) : (
+      <textarea
+        id={props.id}
+        rows={props.rows || 3}
+        onChange={changeHandler}
+        onBlur={touchHandler}
+        value={inputState.value}
+      />
+    );
+
+  // let element;
+  // switch (props.element) {
+  //   case "input":
+  //     element = (
+  //       <input
+  //         id={props.id}
+  //         type={props.type}
+  //         placeholder={props.placeholder}
+  //         onChange={changeHandler}
+  //         onBlur={touchHandler}
+  //         value={inputState.value}
+  //       />
+  //     );
+  //     break;
+  //   case "textarea":
+  //     element = (
+  //       <textarea
+  //         id={props.id}
+  //         rows={props.rows || 3}
+  //         onChange={changeHandler}
+  //         onBlur={touchHandler}
+  //         value={inputState.value}
+  //       />
+  //     );
+  //     break;
+  //   case "select":
+  //     element = (
+  //       <select
+  //         id={props.id}
+  //         onChange={changeHandler}
+  //         onBlur={touchHandler}
+  //         value={inputState.value}
+  //       >
+  //         <option value="" disabled>
+  //           Select your option
+  //         </option>
+  //         <option value="guitar">Guitar</option>
+  //         <option value="bass">Bass</option>
+  //         <option value="fx">FX</option>
+  //       </select>
+  //     );
+  //     break;
+  //   default:
+  //     element = "";
+  // }
+
+  return (
+    <div
+      className={`form-control ${
+        !inputState.isValid && inputState.isTouched && "form-control--invalid"
+      }`}
+    >
+      <label htmlFor={props.id}>{props.label}</label>
+      {element}
+      {!inputState.isValid && inputState.isTouched && <p>{props.errorText}</p>}
+    </div>
+  );
+}
+
+export default Input;
